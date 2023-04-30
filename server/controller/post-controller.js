d46018c29fc3d9662eb6c114a3827102fab1c063
ashish <ashishguptaitself@gmail.com>
2023-04-30 16:23:13 +0530

    Day-5 creating Post api handler and search post from db

--- server/controller/post-controller.js
+++ server/controller/post-controller.js
@@ -1,3 +1,4 @@
+import { request, response } from "express";
 import Post from "../model/post.js";
 
 
@@ -10,5 +11,22 @@ export const createPost = async (request, response) => {
         return response.status(200).json('Post saved successfully');
     } catch (error) {
         return response.status(500).json(error);
+    }
+}
+
+export const getAllPosts = async (request, response) => {
+    let category = request.query.category;
+    let posts;
+    try {
+        if (category) {
+            
+            posts = await Post.find({ categories:category});
+        } else {
+            posts = await Post.find({});
+        }
+
+        return response.status(200).json(posts);
+    } catch (error) {
+        return response.status(500).json({msg:error.message})
     }
 }
